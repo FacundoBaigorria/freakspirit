f21d21f335581b80c74cb640da28267e9b6e8ce8
FacundoBaigorria <facuubaigorria@gmail.com>
2022-03-20 18:33:47 -0300

    1° intento de lanzamiento
    Diseño terminado de home page para git hub

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Funciones y definiciones
+ * 
+ * @package freakspirit
+ * @subpackage freakspirit
+ * @since 1.0
+ */
+
+/**
+ * Definiendo constantes
+ *
+ * @package freakspirit
+ * @subpackage freakspirit
+ * @since 1.0
+ */
+
+define('THEME_ROOT', 'get_stylesheet_directory_uri()');
+
+define('IMAGENES',THEME_ROOT .'/img');
